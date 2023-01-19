345b22b7026b3b864de35d829adda63219bd5c3a
TblRka <92474441+TblRka@users.noreply.github.com>
2023-01-19 19:12:42 +0300

    +unit test

---
+++ HistogramTest/HistogramTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HistogramTest
+{
+	TEST_CLASS(HistogramTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
